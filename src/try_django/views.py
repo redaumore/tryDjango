254f34c04549722514d6a75f902d8ae7e06a6f50
Rolando <redaumore@gmail.com>
2021-04-08 19:39:53 -0300

    Context processors basic

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
-    my_title = "Home Page"
-    return render(request, "hello_world.html", {"title": my_title})
+    my_title = "Hello here"
+    return render(request, "home.html", {"title": my_title})
 
 def about_page(request):
     my_title = "About Page"
@@ -12,3 +13,13 @@ def about_page(request):
 def contact_page(request):
     my_title = "Contact Page"
     return render(request, "hello_world.html", {"title": my_title})
+
+def manually_page():
+    #Handle templates "manually". Only for academic purpouse
+    my_title = "Hello there..."
+    context = {"title": my_title}
+    template_name = "title.txt"
+    template_object = get_template(template_name)
+    rendered_string = template_object.render(context)
+    return render(request, "hello world", {"title": rendered_string}) #load a text located in file title.txt 
+#
