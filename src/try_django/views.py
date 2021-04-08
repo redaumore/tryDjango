76c59dbecbafd18e199f5f3a9432286d1cbfa3d3
Rolando <redaumore@gmail.com>
2021-04-08 14:59:08 -0300

    About & Contacts views

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,3 +2,9 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def about_page(request):
+    return HttpResponse("<h1>Hello About</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Hello Contact</h1>")
